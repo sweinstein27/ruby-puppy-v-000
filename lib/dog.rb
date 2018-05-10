0472ed4b5e49fe938619b65bb6932ed1b8407ff3
Scott Weinstein <sweinstein27@gmail.com>
2018-05-10 14:21:59 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+require "pry"
+
+class Dog
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+def self.clear_all
+  @@all.clear
+end
+
+
+end
